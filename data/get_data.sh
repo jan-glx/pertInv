923602552bca02c69522f450891f4891015b6e46
Jan Gleixner <jan.gleixner@gmail.com>
2017-05-12 16:17:08 +0200

    added script to obtain data from geo database

---
+++ data/get_data.sh
@@ -0,0 +1,6 @@
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE90nnn/GSE90063/suppl/GSE90063_RAW.tar
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE90nnn/GSE90063/suppl/GSE90063_dc0hr_umi_wt.txt.gz 
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE90nnn/GSE90063/suppl/GSE90063_dc3hr_umi_wt.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE90nnn/GSE90063/suppl/GSE90063_k562_umi_wt.txt.gz
+tar -xvf GSE90063_RAW.tar
+rm GSE90063_RAW.tar
