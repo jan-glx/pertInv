cde16d29d9ddde2d2ad823265f3f7c7e7b9b75c7
Jan Gleixner <jan.gleixner@gmail.com>
2017-06-18 22:43:45 +0200

    added script to run ICP on cluster

---
+++ scripts/0_load_n_run.R
@@ -0,0 +1,4 @@
+devtools::install('.')
+source("scripts/1_load_data.R")
+source("scripts/2_filter_data.R")
+source("scripts/3_ICP.R")
--- scripts/3_ICP_try.R
+++ scripts/3_ICP_try.R
@@ -9,43 +9,42 @@ Y_adj <- residuals(fit)
 
 i=3
 YY <- Y_adj[,i]
-#XX <- cbind(model.matrix(~I(CDR^2)+I(CDR^3)+CDR+total_counts_scaled+batch,data=covariates.dt),
- #           Y_adj[,-i])
 XX <-Y_adj[,-i]
-ExpInd <- covariates.dt[, guide] #interaction(batch, guide)]#target_gene]#
-#ExpInd[is.na(ExpInd)] <- "none"
+ExpInd <- covariates.dt[, target_gene] #interaction(batch, guide)]#target_gene]#
+ExpInd[is.na(ExpInd)] <- "none"
 
 #res <- InvariantCausalPrediction::ICP(X, Y, ExpInd,"exact", maxNoObs = 1000, maxNoVariables=12)
 #res
 
-pb = txtProgressBar(min = 0, max = ncol(Y_adj), initial = 0)
+pb = txtProgressBar(min = 0, max = ncol(Y_adj), initial = 0, style=3)
 res = list()
 for (i in seq_len(ncol(Y_adj))) {
   YY <- Y_adj[,i]
   XX <- Y_adj[,-i]
-  res[[i]] <-ICP(XX, YY, ExpInd
+  res[[i]] <-ICP(XX, YY, ExpInd,
                  showAcceptedSets = FALSE,
-                 showCompletion = FALSE,stopIfEmpty=TRUE) #hiddenICP(XX, YY, ExpInd, intercept = TRUE)
+                 showCompletion = FALSE,
+                 stopIfEmpty=TRUE) #hiddenICP(XX, YY, ExpInd, intercept = TRUE)
   setTxtProgressBar(pb,i)
 }
 close(pb)
+save(res, file="ICP_run.Rdata")
 
-
-start_time <- proc.time()
-res <- hiddenICP(XX, YY, ExpInd, intercept = TRUE)
-proc.time()-start_time
-
-start_time <- proc.time()
-res <- ICP(XX, YY, ExpInd)
-proc.time()-start_time
-
-
-start_time <- proc.time()
-res <- ICP(XX, YY, ExpInd, tes="exact")
-proc.time()-start_time
-
-
-library(sisVIVE)
-cv.sisVIVE(YY,XX[,1], model.matrix(~ExpInd))
+# start_time <- proc.time()
+# res <- hiddenICP(XX, YY, ExpInd, intercept = TRUE)
+# proc.time()-start_time
+#
+# start_time <- proc.time()
+# res <- ICP(XX, YY, ExpInd)
+# proc.time()-start_time
+#
+#
+# start_time <- proc.time()
+# res <- ICP(XX, YY, ExpInd, tes="exact")
+# proc.time()-start_time
+#
+#
+# library(sisVIVE)
+# cv.sisVIVE(YY,XX[,1], model.matrix(~ExpInd))
 
 
