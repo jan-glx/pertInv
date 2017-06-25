853a96ededc72d428f64fcca3784c195ab4b447f
Jan Gleixner <jan.gleixner@gmail.com>
2017-06-26 01:26:29 +0200

    added batch execution

--- scripts/0_load_n_run.R
+++ scripts/0_load_n_run.R
@@ -1,4 +1,7 @@
 devtools::install('.')
 source("scripts/1_load_data.R")
 source("scripts/2_filter_data.R")
+
+args <- commandArgs(trailingOnly = TRUE)
+ii <- as.integer(args[1])
 source("scripts/3_ICP.R")
