e155568ef738eddb1f4c608da725649189196398
Jan Gleixner <jan.gleixner@gmail.com>
2017-08-31 11:36:40 +0200

    add plot to compare selection procedures

--- scripts/3_variable_selection comparison.R
+++ scripts/3_variable_selection comparison.R
@@ -15,11 +15,11 @@ subset <- seq_len(if(is_dev()) 8 else ncol(Y_adj))
 glmboost_selection <- data.table(target_gene = colnames(Y_adj)[subset], i=subset)
 glmboost_selection <-
   glmboost_selection[
-    ,.(predictor_gene = names(coef(suppressWarnings(mboost::glmboost(Y_adj[,-i[1]],Y_adj[,i[1]], center = TRUE)))))
-    ,by=target_gene]
+    ,{cat(".");.(predictor_gene = names(coef(suppressWarnings(mboost::glmboost(Y_adj[,-i[1]],Y_adj[,i[1]], center = TRUE)))))
+    },by=target_gene]
 
 
-corr_pval <- psych::corr.test(Y_adj,Y_adj[,subset],adjust="none")$p
+corr_pval <- psych::corr.test(Y_adj,Y_adj[,subset,drop=F],adjust="none")$p
 
 corr_pval <- melt(data.table(corr_pval, keep.rownames=TRUE), variable.factor = FALSE, id.vars = "rn")
 setnames(corr_pval, c("predictor_gene", "target_gene", "p.value"))
@@ -40,3 +40,14 @@ ggplot(corr_pval, aes(x=p.value, fill=factor(selected,levels=c(TRUE,FALSE))))+ge
                           breaks=c(TRUE, FALSE),
                           labels=c("yes", "no"))+xlab("correlation test p-value")
 )
+
+setorder(corr_pval,p.value)
+corr_pval[,fraction_selected := cumsum(selected)]
+
+library(viridis)
+figure("glmboost slection vs. correllation test",
+ggplot(corr_pval, aes(x=order(p.val.adj),color=-log(p.val.adj+.Machine$double.eps),y=fraction_selected))+geom_line(size=2)+
+  xlab("rank correlation p.value")+ylab("cumulative selected by glmboost method")+scale_color_viridis(name="neg. log \ncorrellation p.value\n(BH adjusted)")+
+  theme(legend.position = c(0.8, 0.2))
+)
+
