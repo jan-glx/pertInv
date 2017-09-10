5eff363af2d87bb60ee05060bb14cf48aa624c22
Jan Gleixner <jan.gleixner@gmail.com>
2017-09-10 15:27:43 +0200

    export split violin plot

--- R/common.R
+++ R/common.R
@@ -148,6 +148,7 @@ GeomSplitViolin <- ggproto("GeomSplitViolin", GeomViolin, draw_group = function(
   }
 })
 
+#' @export
 geom_split_violin <- function (mapping = NULL, data = NULL, stat = "ydensity", position = "identity", ..., draw_quantiles = NULL, trim = TRUE, scale = "area", na.rm = FALSE, show.legend = NA, inherit.aes = TRUE) {
   layer(data = data, mapping = mapping, stat = stat, geom = GeomSplitViolin, position = position, show.legend = show.legend, inherit.aes = inherit.aes, params = list(trim = trim, scale = scale, draw_quantiles = draw_quantiles, na.rm = na.rm, ...))
 }
