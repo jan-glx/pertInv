0b24d4e0a05530db9ff26e4b3fbfcf69f3065a93
Jan Gleixner <jan.gleixner@gmail.com>
2017-11-21 13:59:50 +0100

    improve ICP example visualization

--- scripts/0HA_simulate_example_graph.R
+++ scripts/0HA_simulate_example_graph.R
@@ -1,5 +1,6 @@
 library(pertInv)
 library(GGally)
+library(latex2exp)
 
 n <- 300
 dts <- list()
@@ -31,11 +32,13 @@ dts[[e]] <- data.table(e, Y, X_2, X_3, X_4, X_5)
 dt <- rbindlist(dts)
 dt[,e:=as.factor(e)]
 
-#pairs(dt)
-ggpairs(dt, aes(colour = e, alpha = 0.4), axisLabels="none", columns=2:ncol(dt))
 dt[,i:=.I]
-dt[,environment:=sprintf("e=%i",e)]
-dt2 <- melt(dt,id.vars = c("i","e","environment","Y"))[,Y_name:="Y"]
+dt[, environment:=sprintf("e=%i",e)]
+dt[, environment_x:= factor(environment, levels = unique(environment), labels = latex2exp::TeX(unique(environment), output="character"))]
+
+
+dt2 <- melt(dt,id.vars = c("i","e","environment","environment_x","Y"))[,Y_name:="Y"]
+dt2[, variable_x := factor(variable, levels = unique(variable), labels = latex2exp::TeX(unique(variable), output="character"))]
 
 for (j in 1:3) {
   dt3 <-  dt2[as.integer(e)<=j]
@@ -53,16 +56,23 @@ for (j in 1:3) {
 
 
 figure("ICP-Visualization: pairs plot -- individual environments",
-  ggplot(dt2, aes(y=value,x=Y,color=environment))+
+  ggplot(rbind(dt2,copy(dt2)[,environment_x:=latex2exp::TeX("e\\in\\{1,2,3\\}",output="character")]), aes(y=value,x=Y,color=environment))+
     geom_blank(data=dt2)+
-    facet_grid(variable~environment, switch="both") +
+    facet_grid(variable_x~environment_x, labeller = label_parsed) +
     geom_point() +
-    geom_smooth(method="lm", fullrange=TRUE) + theme_void() +
+    geom_smooth(method="lm", fullrange=TRUE) +
     theme(#legend.justification = c(1, 1),
           #legend.position = c(1, 1),
+          legend.position="none",
+          axis.text.x=element_blank(),
+         # axis.title.y=element_blank(),
+          axis.text.y=element_blank(),
+         # axis.ticks.y=element_blank(),
+         # axis.line.y= element_blank(),
           strip.background = element_rect(fill="gray",color=NA)
-    ) +   scale_color_manual(values=cbPalette[-1]),
-  width=5, height=8
+    ) +   scale_color_manual(values=cbPalette[-1]) +
+    ylab(expression(X[i])),
+  width=6, height=6
 )
 
 dt2[,dist:="Y"]
@@ -112,12 +122,10 @@ figure("ICP-Visualization: pairs plot--adj",
          ) +   scale_color_manual(values=cbPalette[-1]),
        width=7, height=8
 )
-library(latex2exp)
-
-dt2[,Y_adj:=residuals(lm(Y~value)), by=.(e,variable)]
 
 dt2[, variable_int := as.integer(variable)]
-dt3 <- dt2[dt2, .(S=paste0("S=\\{", variable, ",", i.variable, "\\}\\} "),
+
+dt3 <- dt2[dt2, .(S=paste0("S=\\{", variable, ",", i.variable, "\\} "),
                   S1_name = variable, S2_name = i.variable,
                   S1 = value, S2 = i.value,
                   Y_adj_name = sprintf("Y-E[Y|%s,%s]",variable, i.variable),
@@ -126,41 +134,19 @@ dt3 <- dt2[dt2, .(S=paste0("S=\\{", variable, ",", i.variable, "\\}\\} "),
              Y_adj := residuals(lm(Y~S1+S2)), by=S
              ]
 
-dt2[,':='(S=paste0("S=\\{", variable,"\\}\\} "),
+dt2[,':='(S=paste0("S=\\{", variable,"\\}"),
       S1_name = variable,
       S1 = value,
       Y_adj_name = sprintf("Y-E[Y|%s]",variable),
       Y_adj = residuals(lm(Y~value))), by= variable]
-dt[,':='(S="S=\\{ \\}\\} ",
+dt[,':='(S="S=\\{ \\}",
       Y_adj_name = "Y-E[Y]",
       Y_adj = residuals(lm(Y~1)))]
 
 
-figure("ICP-Visualization: |S|=2",
-       ggplot(dt3, aes(y=Y_adj, x="", color=environment))+
-         facet_grid(environment~latex2exp::TeX(S), scales="free_y") +
-         geom_jitter_normal(alpha=0.5, stroke=0, width=0.2) +
-         geom_boxplot(fill=NA, color="black") +
-         ylab(expression(Y-hat(Y)[S])) + geom_abline(slope=0, linetype="dashed",color=cbPalette[1]) +
-         theme(axis.text.x=element_blank(),
-               axis.title.y=element_blank(),
-               axis.text.y=element_blank(),
-               axis.ticks.y=element_blank(),
-               axis.line.y= element_blank(),
-               #legend.justification = c(1, 1),
-           #legend.position = c(1.1, 1.1),
-           legend.position="none",
-           strip.background = element_rect(fill="gray",color=NA)
-         ) +   scale_color_manual(values=cbPalette[-1]) + coord_flip() ,
-       width=8, height=3
-)
-
-
 dt4 <- rbind(dt,dt2,dt3,fill=TRUE)
-dt4[, S:= factor(S, levels= unique(S), labels=sapply(unique(S), latex2exp::TeX, output="character"))]
-dt4[, environment:= factor(environment, levels= unique(environment), labels=sapply(unique(environment), latex2exp::TeX, output="character"))]
-
-
+dt4[, S:= factor(S, levels = unique(S), labels = latex2exp::TeX(unique(S), output="character"))]
+dt4[, environment:= factor(environment, levels = unique(environment), labels = latex2exp::TeX(unique(environment), output="character")))]
 
 figure("ICP-Visualization: |S|<=2",
        ggplot(dt4, aes(y=Y_adj, x="", color=environment))+
@@ -178,5 +164,5 @@ figure("ICP-Visualization: |S|<=2",
                legend.position="none",
                strip.background = element_rect(fill="gray",color=NA)
          ) +   scale_color_manual(values=cbPalette[-1]) + coord_flip() ,
-       width=10, height=3
+       width=12, height=3
 )
