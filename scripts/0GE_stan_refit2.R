1ea180e3b55385f4e03059f4c505b5281560b68e
Jan Gleixner <jan.gleixner@gmail.com>
2017-12-12 00:27:24 +0100

    backup

--- scripts/0GE_stan_refit2.R
+++ scripts/0GE_stan_refit2.R
@@ -1,3 +1,5 @@
+
+# ---------------------------
 library(pertInv)
 library(rstan)
 library(shinystan)
@@ -18,26 +20,188 @@ ee <- list()
 dat <- list(n_c = 64, n_g = 32, n_r = 16);
 
 ii <- 1
-mm[[ii]] <-  stan_model_builder("stan_lib/1_simulate.stan")
+mm[[ii]] <-  stan_model_builder("stan_lib/1C_simulate.stan")
 fit_mc[[ii]] <- sampling(mm[[ii]], data = dat, chains=1, iter = 1000, warmup=0, algorithm="Fixed_param")
 ee[[ii]] <- extract(fit_mc[[ii]])
 
 dat  <-  c(dat, index_sample(ee[[ii]], i=1))
 
 ii <- 2
-mm[[ii]] <-  stan_model_builder("stan_lib/1_fit.stan")
+mm[[ii]] <-  stan_model_builder("stan_lib/1C_fit_vec_nb_disc.stan")
 fit_mc[[ii]] <- sampling(mm[[ii]], data = dat)
-cat(fit_mc[[ii]]@stanmodel@model_code, file="stan_lib/to_read.stan")
-ee[[ii]] <- extract(fit_mc[[ii]])
-list2env(dat,globalenv())
+fit_vb[[ii]] <- vb(mm[[ii]], data = dat)
 
-# check predictions
-for (i in names(ee[[ii]])) {
-  tmp = ee[[ii]][[i]]
-  dim(tmp) = c(dim(tmp)[1],prod(dim(tmp)[-1]))
-  hist(tmp[,1], main=i)
-  abline(v=dat[[i]][1],col="red")
+
+
+ee[[ii]] <- extract(fit_vb[[ii]])
+ee[[ii]] <- extract(fit_mc[[ii]])
+datt <- lapply(index_sample(ee[[ii]], i=1),function(x){
+  x <- as.array(x)
+  if(length(x)>1) {
+    dim(x)<-c(1,dim(x))
+  }
+  x})
+
+summarize_sample <- function(xx) {
+  xx <- xx[!names(xx)%in% c("D","lp__","R","Y")]
+  xx <- lapply(xx,function(x){
+    if(!is.null(dim(x))&&length(dim(x))>1) {
+      dim(x) <- c(dim(x)[1],prod(dim(x)[-1]))
+    }
+    x
+    })
+  melt(as.data.table(xx),id.vars=integer(0))[,.(mean=mean(value),median=median(value),lower=quantile(value,0.025),upper=quantile(value,0.975)),by=variable]
 }
+
+
+
+
+dt <- rbind(
+  summarize_sample(extract(fit_mc[[1]]))[,method:="prior"],
+  summarize_sample(extract(fit_vb[[ii]]))[,method:="VB"],
+  summarize_sample(extract(fit_mc[[ii]]))[,method:="MC"],
+  summarize_sample(datt)[,method:="ground truth"]
+  )
+
+dt[,c("variable_class","number"):=tstrsplit(variable,'.V',fixed=TRUE,keep=1:2)]
+dt[,number:=as.integer(number)]
+dt[is.na(number),number:=0]
+
+figure("parameter recovery VBMC",
+ggplot(dt[number<11], aes(x=variable,y=mean,ymin=lower,ymax=upper,color=method))+
+  geom_errorbar(width=0,position=position_dodge(0.2))+
+  geom_point(position=position_dodge(0.2))+
+  facet_wrap("variable_class",scales="free")+
+  theme(axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        legend.justification = c(1, 0),
+        legend.position = c(1, 0),
+        legend.title=element_blank())+
+  ylab("value")+coord_flip(),
+width=9,height=5)
+
+
+dt.single <- dt[!(variable_class %in% dt[number==3, unique(variable_class)])]
+
+
+figure("1D parameter recovery VB/MC",
+ggplot(dt.single, aes(x=variable,y=mean,ymin=lower,ymax=upper,color=method))+
+  geom_rect(data=dt.single[method=="prior"], xmin=-Inf,xmax=Inf,alpha=0.3,fill="gray",color=NA)+
+  geom_hline(data=dt.single[method=="prior"], aes(yintercept=mean),color="gray")+
+  geom_crossbar(data=dt.single[method %in% "ground truth"],aes(y=mean,color=method))+
+  geom_errorbar(data=dt.single[method %in% c("VB","MC")],width=0,position=position_dodge(0.3))+
+  geom_point(data=dt.single[method %in% c("VB","MC")],position=position_dodge(0.3))+
+  facet_wrap("variable",scales="free", nrow=2)+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        legend.title=element_blank())+
+  ylab("value"),
+width=12,height=9)
+
+
+
+
+dt.multi <- dt[(variable_class %in% dt[number==3, unique(variable_class)])][number<11]
+dt.multi[,variable_factor:=factor(variable,levels=varsorted),]
+dt.multi[dt.multi[method=="ground truth",.(variable_coded=rank(mean),variable),by=variable_class],variable_coded:=variable_coded,on="variable"]
+#varsorted <- setorder(unique(dt.multi[method=="ground truth"], key="variable"),"mean")[,variable]
+#dt.multi[,variable_factor:=factor(variable,levels=varsorted)]
+dt.prior <- dt.multi[method=="prior"][,lapply(.SD,mean),by=.(method,variable_class), .SDcols=c("mean","median","lower","upper")]
+dt.prior[,variable_coded:=1L]
+
+figure("n-D parameter recovery VB/MC",
+       ggplot(dt.multi, aes(x=variable_coded,y=mean,ymin=lower,ymax=upper,color=method))+
+         geom_point(data=dt.multi[method %in% c("VB","MC")],position=position_dodge(0.3))+
+      #   geom_rect(data=dt.prior, xmin=-Inf, xmax=Inf, alpha=0.3,fill="gray",color=NA)+
+         geom_hline(data=dt.prior, aes(yintercept=mean),color="gray")+
+           geom_rect(data=dt.prior, xmin=-Inf, xmax=Inf, alpha=0.3,fill="gray",color=NA)+
+         geom_crossbar(data=dt.multi[method %in% "ground truth"],aes(y=mean,color=method))+
+         geom_errorbar(data=dt.multi[method %in% c("VB","MC")],width=0,position=position_dodge(0.3))+
+         geom_point(data=dt.multi[method %in% c("VB","MC")],position=position_dodge(0.3))+
+         facet_wrap("variable_class",scales="free", nrow=2)+
+        scale_x_discrete(limits=varsorted)+
+         theme(axis.title.x=element_blank(),
+               axis.text.x=element_blank(),
+               axis.ticks.x=element_blank(),
+               legend.title=element_blank())+
+         ylab("value"),
+       width=12,height=9)
+
+
+# initialization from prior
+inits <- index_samples(ee[[1]], sample(dim((ee[[1]])[[1]])[1]-1,4)+1)
+
+ii <- 3
+mm[[ii]] <-  stan_model_builder("stan_lib/1C_fit_vec_nb_disc.stan")
+fit_mc[[ii]] <- sampling(mm[[ii]], data = dat, init=inits)
+fit_vb[[ii]] <- vb(mm[[ii]], data = dat, output_samples=100, tol_rel_obj=0.001)
+ee[[ii]] <- extract(fit_vb[[ii]])
+
+lp__vb <- function(fit_vb, e_vb=extract(fit_vb)) {
+  sapply(seq_len(dim(e_vb[[1]])[1]), function(i) {
+    pars <- index_sample(e_vb, i)
+    log_prob(fit_vb, unconstrain_pars(fit_vb, pars))
+    })
+}
+
+lp__vb(fit_vb[[ii]] )
+
+# initialize knockout matrix with guide matrix
+R_true <- dat$R
+
+dat$R <- dat$D
+
+fit_vb_tm1 <- vb(mm[[ii]], data = dat, output_samples=100, tol_rel_obj=0.001)
+#lp_tm1 <-  lp__vb(fit_vb_tm1)
+#lp_R_term_tm1 <-  t(extract(fit_vb_tm1, pars="lp_R_terms")[[1]][1,,])
+
+Rt <- list()
+n <- 10
+p_acceptance <- numeric(n)
+is <- integer(n)
+output <- list()
+for (t in seq_len(n)){
+
+  Rt[[t]] <- as.logical(dat$R)
+  output[[t]] <- capture.output(
+    fit_vb_tm1 <- vb(mm[[ii]], data = dat, output_samples=100, tol_rel_obj=0.001,eta=1)
+  )
+  i <- 11 #sample(dat$n_r*dat$n_c, 1)
+  is[t] <- i
+  dat$R[i] <- !dat$R[i]
+  output[[t]] <- c(output[[t]] ,capture.output(
+    fit_vb_t <- vb(mm[[ii]], data = dat, output_samples=100, tol_rel_obj=0.001,eta=1)
+  ))
+
+  #lp_t <- lp__vb(fit_vb_t)
+  #lp_R_term_t <- t(extract(fit_vb_t, pars="lp_R_terms")[[1]][1,,])
+  #p_acceptance[t] <- 1/(1+exp(lp_R_term_tm1[i]-lp_R_term_t[i]))
+  #p_acceptance[t] <- exp(lp_R_term_t[i])
+  lp_D_X1_Z1 <- lp__vb(fit_vb_t, extract(fit_vb_t))
+  lp_D_X0_Z0 <- lp__vb(fit_vb_tm1, extract(fit_vb_tm1))
+  lp_D_X1_Z0 <- lp__vb(fit_vb_tm1, extract(fit_vb_t))
+  lp_D_X0_Z1 <- lp__vb(fit_vb_t, extract(fit_vb_tm1))
+
+  p_acceptance[t] <- m
+  accpeted <- runif(1)< exp(p_acceptance[t])
+  if (accpeted) {
+    cat(":")
+    #lp_tm1 <- lp_t
+    #fit_vb_tm1 <- fit_vb_t
+    #lp_R_term_tm1 <- lp_R_term_t
+  } else {
+    cat(".")
+    dat$R[i] <- !dat$R[i]
+  }
+}
+dat$R <- R_true
+
+#check runtimes
+lapply(fit_mc, function(fit_mc) rowMeans(sapply(fit_mc@sim$samples, function(sample) attr(sample, "elapsed_time"))))
+
+
 
 
 ii <- 3
@@ -110,3 +274,31 @@ dat$n_r  <-  ncol(dat$D) # number of sgRNAs
 n <- dat$n_c * dat$n_g
 dat$ii_test <-  sample(n, ceiling(n/5))
 dat$n_test <- length(dat$ii_test)
+
+
+
+
+
+# ----------------- snippets
+
+# show merged source
+cat(fit_mc[[ii]]@stanmodel@model_code, file="stan_lib/to_read.stan")
+
+# attach data to globalenv for shinystan to find
+list2env(dat,globalenv())
+
+# check predictions with histogramms # deprecated
+for (i in names(ee[[ii]])) {
+  tmp = ee[[ii]][[i]]
+  dim(tmp) = c(dim(tmp)[1],prod(dim(tmp)[-1]))
+  hist(tmp[,1], main=i)
+  abline(v=dat[[i]][1],col="red")
+}
+
+#shinystan subset to not n_c x n_g parameters
+sstan = as.shinystan(fit_mc[[ii]], pars=fit_mc[[ii]]@sim$pars_oi[!(fit_mc[[ii]]@sim$pars_oi %in% c("X", "X_train", "X_test"))])
+shinystan::launch_shinystan(sstan)
+
+
+lapply(ee[[ii]], mean)
+
