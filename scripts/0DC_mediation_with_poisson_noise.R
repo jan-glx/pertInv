ea368500fa4d24dd54620be6df34b15dd60ee43d
Jan Gleixner <jan.gleixner@gmail.com>
2018-01-05 22:15:50 +0100

    add simple simulation to demonstrate effect of noise on mediation analysis

--- scripts/0DC_mediation_with_poisson_noise.R
+++ scripts/0DC_mediation_with_poisson_noise.R
@@ -14,3 +14,14 @@ ppcor::pcor.test(A,B,c_measured) # significant
 # I am looking for some thing like that:
 ppcor::pcor.test(A,B,c_measured, z_noise_sd=sd_noise) # not significant
 
+
+dat <- list(n=n, mA=mA, mB=mB, mC=mC)
+stan_model <- stan_model_builder("stan_lib/2_mediation_poisson.stan")
+#fit_vb <- vb(mm[[ii]], data = dat, adapt_engaged=FALSE, eta=0.1, tol_rel_obj=0.001)
+fit <- sampling(stan_model, data = dat)
+ee <- extract(fit)
+summarize_sample(ee)[variable %in% c("beta_AB","beta_AC","beta_BC")]
+# variable        mean      median      lower     upper
+# 1:  beta_AB  0.49516446  0.49437076  0.4002955 0.5969427
+# 2:  beta_AC -0.05567849 -0.04537204 -0.2745368 0.1256840
+# 3:  beta_BC  0.54754365  0.53296445  0.2813668 0.9120366
